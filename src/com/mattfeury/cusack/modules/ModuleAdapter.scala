9ef7cda5b03c2fdde70675809a3336c4071ae3c0
Matt Feury <mattfeury@gmail.com>
2014-04-06 22:48:18 -0400

    Don't cache views in the ModuleAdapter
    this may cause performance issues, but since each module may use a
    different layout, then we can't share them

--- src/com/mattfeury/cusack/modules/ModuleAdapter.scala
+++ src/com/mattfeury/cusack/modules/ModuleAdapter.scala
@@ -12,13 +12,12 @@ import android.widget.ArrayAdapter
 // ResourceID isn't really honored here...
 case class ModuleAdapter(context:Context, resourceId:Int, items:List[Module[Cusack]]) extends ArrayAdapter(context, resourceId, items.toArray) {
     override def getView(position:Int, convertView:View, parent:ViewGroup) : View = {
-        var view = convertView
         val module = getItem(position)
 
-        if (view == null) {
-            def inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE).asInstanceOf[LayoutInflater]
-            view = inflater.inflate(module.templateId, null)
-        }
+        // We don't allow the adapter to cache views here since each module may use a different layout.
+        // This may cause performance impacts but since we shouldn't have too many modules, we should be alright.
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE).asInstanceOf[LayoutInflater]
+        val view = inflater.inflate(module.templateId, null)
 
         module.render(view)
 
