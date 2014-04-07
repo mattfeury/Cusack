0234caa6444b7e830e7c1696e2a4f72d281ebd63
Matt Feury <mattfeury@gmail.com>
2014-04-07 02:14:02 -0400

    Url encode rap genius query params

--- src/com/mattfeury/cusack/modules/LyricsModule.scala
+++ src/com/mattfeury/cusack/modules/LyricsModule.scala
@@ -3,11 +3,11 @@ package com.mattfeury.cusack.modules
 import com.mattfeury.cusack.CusackReceiver
 import com.mattfeury.cusack.R
 import com.mattfeury.cusack.music.Song
-
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.TextView
+import com.mattfeury.cusack.util.Utils
 
 class LyricsModule[A <: CusackReceiver with Context](receiver:A, attrs:AttributeSet = null) extends Module(receiver, attrs) {
     override def selected = {
@@ -15,8 +15,8 @@ class LyricsModule[A <: CusackReceiver with Context](receiver:A, attrs:Attribute
     }
 
     def sendToRapGenius(song:Song) = {
-        // TODO urlEncode
-        receiver.openURIIntent("http://rapgenius.com/search?q=" + song.artist + " " + song.name)
+        val params = Utils.makeQueryString(Map(("q", song.artist.name + " " + song.name)))
+        receiver.openURIIntent("http://rapgenius.com/search?" + params)
     }
 
     override def render(view:View) = {
