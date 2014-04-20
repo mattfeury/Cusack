ea9d94cb71cf2888627f9c211f5ff1720c76f948
Matt Feury <mattfeury@gmail.com>
2014-04-19 20:42:30 -0400

    Add about dialog menu item

--- src/com/mattfeury/cusack/Cusack.scala
+++ src/com/mattfeury/cusack/Cusack.scala
@@ -1,18 +1,26 @@
 package com.mattfeury.cusack
 
-import com.mattfeury.cusack.modules._
-import com.mattfeury.cusack.services.SongDetector
+import com.mattfeury.cusack.modules.ImageModule
+import com.mattfeury.cusack.modules.LastFmBioModule
+import com.mattfeury.cusack.modules.LyricsModule
+import com.mattfeury.cusack.modules.ModuleAdapter
+import com.mattfeury.cusack.modules.SongInfoModule
+import com.mattfeury.cusack.modules.TwitterModule
+import com.mattfeury.cusack.modules.WikipediaModule
+import com.mattfeury.cusack.music.NowPlaying
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.app.Dialog
+import android.app.DialogFragment
+import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.support.v4.app.FragmentActivity
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.ListView
-import android.widget.TextView
-import android.util.Log
-import com.mattfeury.cusack.music.NowPlaying
-import com.mattfeury.cusack.music.Song
 
 trait CusackReceiver <: Activity {
     var adapter:Option[ModuleAdapter] = None
@@ -31,7 +39,7 @@ object Cusack {
     final def TAG = "CUSACK"
 }
 
-class Cusack extends Activity with CusackReceiver {
+class Cusack extends FragmentActivity with CusackReceiver {
 
     override def onCreate(savedInstanceState:Bundle) = {
         super.onCreate(savedInstanceState)
@@ -67,4 +75,39 @@ class Cusack extends Activity with CusackReceiver {
         getMenuInflater().inflate(R.menu.activity_cusack, menu)
         true
     }
+
+    def showAbout(menuItem:MenuItem) = {
+        new AboutDialogFragment().show(getFragmentManager, "about")
+    }
+}
+
+// TODO get this outta here
+class AboutDialogFragment extends DialogFragment {
+    override def onCreateDialog(savedInstanceState:Bundle) : Dialog = {
+        val builder = new AlertDialog.Builder(getActivity(), AlertDialog.THEME_HOLO_DARK)
+
+        builder
+            .setMessage(R.string.about_text)
+            .setPositiveButton("Coolio", DialogClickHandler((d, id) => {}))
+            .setNeutralButton("Cher\nthe app", DialogClickHandler((dialog, id) => {
+                val sendIntent = new Intent()
+                sendIntent.setAction(Intent.ACTION_SEND)
+                sendIntent.putExtra(Intent.EXTRA_TEXT, R.string.share_text)
+                sendIntent.setType("text/plain")
+                startActivity(sendIntent)
+            }))
+            .setNegativeButton("Leave\nsome feedback", DialogClickHandler((dialog, id) => {
+                val emailIntent = new Intent(android.content.Intent.ACTION_SEND)
+                emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, Array(getResources.getString(R.string.feedback_email_address)))
+                emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, Array(getResources.getString(R.string.feedback_email_subject)))
+                emailIntent.setType("text/plain")
+                startActivity(Intent.createChooser(emailIntent, "Send feedback email..."))
+            }))
+
+        builder.create()
+    }
+}
+
+case class DialogClickHandler(fn:(DialogInterface, Int) => Unit) extends DialogInterface.OnClickListener() {
+    def onClick(dialog:DialogInterface, id:Int) = fn(dialog, id)
 }
