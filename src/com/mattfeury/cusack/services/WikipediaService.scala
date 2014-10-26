b915b87cb41a8a197f700b5eee2907f5d47ae5ca
Matt Feury <mattfeury@gmail.com>
2014-10-26 14:41:09 -0400

    Change wikipedia summary parser to use different endpoint
    wikipedia seems like a crapshoot, but the official musicbrainz app uses
    this technique and they seem to have better results. it involves some
    nasty html regex (barf) but if it fits, i uses its

--- src/com/mattfeury/cusack/services/WikipediaService.scala
+++ src/com/mattfeury/cusack/services/WikipediaService.scala
@@ -1,14 +1,12 @@
 package com.mattfeury.cusack.services
 
 import java.net.URLEncoder
-
 import scala.Option.option2Iterable
 import scala.collection.JavaConversions.asScalaIterator
-
 import org.json.JSONArray
 import org.json.JSONObject
-
 import com.mattfeury.cusack.util.Utils
+import java.net.URLDecoder
 
 case class WikipediaPageInfo(title:String, extract:String) {
     def getUrl() = WikipediaService.getUrlForTitle(title)
@@ -37,29 +35,26 @@ trait WikipediaService {
 
     def getPageInfoForTitle(title:String) : Option[WikipediaPageInfo] = {
         makeUrlCall(Map(
-            ("action", "query"),
-            ("prop", "extracts"),
-            ("exintro", ""),
-            ("titles", title)
+            ("action", "mobileview"),
+            ("prop", "text"),
+            ("sections", "0"), // we can change this!
+            ("page", title)
         ), response => {
             for {
                 response <- response
                 json = new JSONObject(response)
-                query = json.getJSONObject("query")
-                pages = query.getJSONObject("pages")
-                page <- pages.keys().asInstanceOf[java.util.Iterator[String]].toList.headOption
-                pageObject = pages.getJSONObject(page)
-                extract = pageObject.get("extract")
-                title = pageObject.get("title")
+                mobileview = json.getJSONObject("mobileview")
+                section <- Utils.jsonArrayToList[JSONObject](mobileview.getJSONArray("sections")).headOption
+                extract = section.get("text")
             } yield {
-                WikipediaPageInfo(title = title.toString, extract = Utils.stripHtml(extract.toString()))
+                WikipediaPageInfo(title = title.toString, extract = Utils.stripHtml(stripExtract(extract.toString())))
             }
         })
     }
 
     def getPageTitleFromUrl(url:String) : String = {
         // Hmm...
-        url.split("/").last
+        URLDecoder.decode(url.split("/").last)
     }
 
     def getPageInfoForKeyword(keyword:String) : Option[WikipediaPageInfo]= {
@@ -86,6 +81,12 @@ trait WikipediaService {
             }
         })
     }
+
+    private def stripLinksAndRefs(input:String): String = input.replaceAll("\\<a.*?>|</a>|\\[.*?\\]", "")
+    private def stripImageTags(input:String) : String = input.replaceAll("\\<img.*?/>", "")
+    private def stripTablesAndDivs(input:String) : String = input.replaceAll("(?s:\\<div.*?</div>|\\<table.*?</table>)", "")
+
+    private def stripExtract(input:String) = stripLinksAndRefs(stripImageTags(stripTablesAndDivs(input))) // Eww stop this 
 
     private def makeUrlCall[T](params:Map[String, String], callback:Option[String]=>T) : T = {
         val url = URL + "?" + Utils.makeQueryString(params + ("format" -> "json"))
